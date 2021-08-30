ac91e60497f763a0a93e7fdec3e4504167f7fea2
Karina Khismatulina <hkarinaf@gmail.com>
2021-08-30 23:35:30 +0300

    нерабочая zalupa а я запуталасб

--- TelegramNoteBot/Handlers.cs
+++ TelegramNoteBot/Handlers.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InlineQueryResults;
-using Telegram.Bot.Types.InputFiles;
 using Telegram.Bot.Types.ReplyMarkups;
 using TelegramNoteBot;
 
@@ -23,7 +19,6 @@ namespace Telegram.Bot.Examples.Echo
     {
         private static long noteId = 0;
         private NoteRepository _noteRepository;
-        //private ConcurrentDictionary<long, UserState> _usersState;
         private UserInfo _userInfo;
         public Handlers(NoteRepository noteRepository)
         {
@@ -46,18 +41,9 @@ namespace Telegram.Bot.Examples.Echo
         {
             var handler = update.Type switch
             {
-                // UpdateType.Unknown:
-                // UpdateType.ChannelPost:
-                // UpdateType.EditedChannelPost:
-                // UpdateType.ShippingQuery:
-                // UpdateType.PreCheckoutQuery:
-                // UpdateType.Poll:
                 UpdateType.Message => BotOnMessageReceived(botClient, update.Message),
                 UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage),
                 UpdateType.CallbackQuery => BotOnCallbackQueryReceived(botClient, update.CallbackQuery),
-                UpdateType.InlineQuery => BotOnInlineQueryReceived(botClient, update.InlineQuery),
-                UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(botClient, update.ChosenInlineResult),
-                _ => UnknownUpdateHandlerAsync(botClient, update)
             };
 
             try
@@ -84,7 +70,6 @@ namespace Telegram.Bot.Examples.Echo
                 var action = (message.Text.Split(' ').First()) switch
                 {
                     "/inline" => SendInlineKeyboard(botClient, message),
-                    "/keyboard" => SendReplyKeyboard(botClient, message),
                     "/remove" => RemoveKeyboard(botClient, message),
                     _ => Usage(botClient, message)
                 };
@@ -117,23 +102,6 @@ namespace Telegram.Bot.Examples.Echo
 
             }
 
-            async Task<Message> SendReplyKeyboard(ITelegramBotClient botClient, Message message)
-            {
-                var replyKeyboardMarkup = new ReplyKeyboardMarkup(
-                    new KeyboardButton[][]
-                    {
-                        new KeyboardButton[] { "1.1", "1.2" },
-                        new KeyboardButton[] { "2.1", "2.2" },
-                    })
-                {
-                    ResizeKeyboard = true
-                };
-
-                return await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
-                                                            text: "Choose",
-                                                            replyMarkup: replyKeyboardMarkup);
-            }
-
             async Task<Message> RemoveKeyboard(ITelegramBotClient botClient, Message message)
             {
                 return await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
@@ -145,7 +113,6 @@ namespace Telegram.Bot.Examples.Echo
             {
                 const string usage = "Usage:\n" +
                                      "/inline   - send inline keyboard\n" +
-                                     "/keyboard - send custom keyboard\n" +
                                      "/remove   - remove custom keyboard\n";
 
                 return await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
@@ -163,7 +130,6 @@ namespace Telegram.Bot.Examples.Echo
                 "createNotesCallback" => CreateNewNote()
                 //_ => botClient.SendTextMessageAsync(chatId: callbackQuery.Message.Chat.Id, text: "tatat")
             };
-            var sentMessage = await action;
 
             async Task<Message> TellMeAboutFunctional()   //=> botClient.SendTextMessageAsync(chatId: callbackQuery.Message.Chat.Id, "создание заметок и пока что всё");
             {
@@ -173,55 +139,44 @@ namespace Telegram.Bot.Examples.Echo
             async Task<Message> CreateNewNote()
             {
                 _userInfo.isComand = false;
-                await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "Введите свою заметку", ParseMode.Default, null, false, false, 0, replyMarkup: new ForceReplyMarkup { Selective = true });
-                while (true)
+                await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "Введите свою заметку", replyMarkup: new ForceReplyMarkup { Selective = true });
+                if (callbackQuery.Data != null && callbackQuery.Message.ReplyToMessage.Text.Contains("Введите свою заметку"))
                 {
-                    if (callbackQuery.Message.ReplyToMessage != null && callbackQuery.Message.ReplyToMessage.Text.Contains("Введите свою заметку"))
-                    {
-                        Note note = new Note(_userInfo.id, ++noteId, callbackQuery.Message.Text, false);
-                        _noteRepository.AddNewNote(note);
-                        _userInfo.isComand = true;
-                        return await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id,
-                                                                    text: "Заметка сохранена!");
-                    }
+                    await botClient.OnMessage += OnMessageHandler;
+                    Note note = new Note(_userInfo.id, ++noteId, callbackQuery.Message.Text, false);
+                    _noteRepository.AddNewNote(note);
+                    _userInfo.isComand = true;
+                    return await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id,
+                                                                text: "Заметка сохранена!");
                 }
                 return await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id,
                                                                 text: "миша всё хуйня давай по-новой");
             }
-        }
 
-        private async Task BotOnInlineQueryReceived(ITelegramBotClient botClient, InlineQuery inlineQuery)
-        {
-            Console.WriteLine($"Received inline query from: {inlineQuery.From.Id}");
+            async Task OnMessageHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            {
+                var handler = update.Type switch
+                {
+                    UpdateType.Message => AddNewNote(update.Message.Text),
+                    _ => botClient.SendAnimationAsync(callbackQuery.Message.Chat.Id, "Введите заметку!")
+                };
 
-            InlineQueryResultBase[] results = {
-                // displayed result
-                new InlineQueryResultArticle(
-                    id: "3",
-                    title: "TgBots",
-                    inputMessageContent: new InputTextMessageContent(
-                        "hello"
-                    )
-                )
-            };
+                try
+                {
+                    await handler;
+                }
+                catch (Exception exception)
+                {
+                    await HandleErrorAsync(botClient, exception, cancellationToken);
+                }
+            }
 
-            await botClient.AnswerInlineQueryAsync(
-                inlineQueryId: inlineQuery.Id,
-                results: results,
-                isPersonal: true,
-                cacheTime: 0);
-        }
-
-        private Task BotOnChosenInlineResultReceived(ITelegramBotClient botClient, ChosenInlineResult chosenInlineResult)
-        {
-            Console.WriteLine($"Received inline result: {chosenInlineResult.ResultId}");
-            return Task.CompletedTask;
-        }
-
-        private Task UnknownUpdateHandlerAsync(ITelegramBotClient botClient, Update update)
-        {
-            Console.WriteLine($"Unknown update type: {update.Type}");
-            return Task.CompletedTask;
+            void AddNewNote(String text)
+            {
+                Note note = new Note(_userInfo.id, ++noteId, callbackQuery.Message.Text, false);
+                _noteRepository.AddNewNote(note);
+                _userInfo.isComand = true;
+            }
         }
     }
 }
